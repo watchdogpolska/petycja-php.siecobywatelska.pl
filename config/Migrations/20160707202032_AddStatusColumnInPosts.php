afc52402199c49bb2a3a1243c2d50f4f8169709d
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-07-07 22:21:03 +0200

    Add status column migration

---
+++ config/Migrations/20160707202032_AddStatusColumnInPosts.php
@@ -0,0 +1,15 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddStatusColumnInPosts extends AbstractMigration
+{
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}
+
