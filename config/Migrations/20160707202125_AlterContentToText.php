0ecb303f35e017d1574df653c4dce587f782a2f1
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-07-07 22:21:46 +0200

    Change content to text

---
+++ config/Migrations/20160707202125_AlterContentToText.php
@@ -0,0 +1,15 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AlterContentToText extends AbstractMigration
+{
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}
+
