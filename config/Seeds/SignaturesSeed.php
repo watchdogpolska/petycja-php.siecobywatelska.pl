106ca86c06e688e5f3cc0df0fa6cf585f7865550
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-07-08 00:33:51 +0200

    Seed for signatures

---
+++ config/Seeds/SignaturesSeed.php
@@ -0,0 +1,39 @@
+<?php
+use Migrations\AbstractSeed;
+
+/**
+ * Signatures seed.
+ */
+class SignaturesSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = $faker = Faker\Factory::create();
+
+        $data = [];
+
+        for ($i=0; $i < 100; $i++) {
+            $data[] = [
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => $faker->safeEmail,
+                'comment' => implode('. ', $faker->sentences($faker->numberBetween(1, 4))),
+                'newsletter' => $faker->randomElement([1, 0]),
+                'created' => $faker->iso8601()
+            ];
+        };
+
+        $table = $this->table('signatures');
+        $table->insert($data)->save();
+    }
+}
---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,52 @@
+<?php
+use Migrations\AbstractSeed;
+use Cake\Auth\DefaultPasswordHasher;
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $hasher = new DefaultPasswordHasher();
+        $faker = $faker = Faker\Factory::create();;
+        $data = [
+            [
+                'login' => 'user1',
+                'email' => 'user1@example.com',
+                'password' => $hasher->hash('password')
+            ],
+            [
+                'login' => 'user2',
+                'email' => 'user2@example.com',
+                'password' => $hasher->hash('password')
+            ],
+            [
+                'login' => 'user3',
+                'email' => 'user3@example.com',
+                'password' => $hasher->hash('password')
+            ]
+        ];
+
+        for ($i=0; $i < 100; $i++) {
+            $data[] = [
+                'login' => $faker->lastName . $faker->numberBetween(1, 5555),
+                'email' => $faker->safeEmail,
+                'password' => $hasher->hash('password')
+            ];
+        }
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
