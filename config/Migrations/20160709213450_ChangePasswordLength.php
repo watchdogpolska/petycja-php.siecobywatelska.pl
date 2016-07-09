46e344bfb1139a0ef0f70ee5b0cea5294a7be41f
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-07-09 23:38:22 +0200

    Extending the column with password

---
+++ config/Migrations/20160709213450_ChangePasswordLength.php
@@ -0,0 +1,29 @@
+<?php
+use Migrations\AbstractMigration;
+
+class ChangePasswordLength extends AbstractMigration
+{
+
+    public function up()
+    {
+
+        $this->table('users')
+            ->changeColumn('password', 'string', [
+                'length' => 255,
+            ])
+            ->update();
+    }
+
+    public function down()
+    {
+
+        $this->table('users')
+            ->changeColumn('password', 'string', [
+                'default' => null,
+                'length' => 40,
+                'null' => false,
+            ])
+            ->update();
+    }
+}
+
