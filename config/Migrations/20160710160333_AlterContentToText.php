bda6f522c6e12c8a6d33882eab1bd48e41d12437
ka54mil <ka54mil@wp.pl>
2016-07-10 18:17:34 +0200

    Changed content column type to Longtext

---
+++ config/Migrations/20160710160333_AlterContentToText.php
@@ -0,0 +1,17 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AlterContentToText extends AbstractMigration
+{
+
+    public function change()
+    {
+        $this->table('posts')
+        ->changeColumn('content', 'text',[
+            'default' => null,
+            'limit' => 4294967295,
+            'null' => false
+        ])
+        ->update();
+    }
+}
