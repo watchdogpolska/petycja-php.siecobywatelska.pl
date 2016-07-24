8d099722bdd18bafab9fe9a6c24ae8b096aea14c
mik-laj <kamil.bregula@siecobywatelska.pl>
2016-07-24 11:38:27 +0200

    Add initial user

---
+++ config/Migrations/20160724093507_InsertFirstAdmin.php
@@ -0,0 +1,32 @@
+<?php
+use Migrations\AbstractMigration;
+
+class InsertFirstAdmin extends AbstractMigration
+{
+   /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $hasher = new DefaultPasswordHasher();
+
+        $user = [
+            'login' => 'root',
+            'display_name' => '',
+            'email' => 'user1@example.com',
+            'password' => $hasher->hash('root')
+        ];
+
+        $table = $this->table('users');
+        $table->insert($user);
+        $table->saveData();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->execute('DELETE FROM users WHERE login = \'root\'');
+    }
+}
